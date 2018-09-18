cc84cc616fa8521388885f17c3e755b751ec5c03
Heng Bros <hengbros196@gmail.com>
2018-09-18 19:39:12 +0700

    Fragment with BottomNavigationView and Event

---
+++ app/src/main/java/edu/rupp/ckcc/fragment_demo/HomeFragment.java
@@ -0,0 +1,26 @@
+package edu.rupp.ckcc.fragment_demo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class HomeFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragmentlayout=inflater.inflate(R.layout.home_fragment,container,false);
+        return fragmentlayout;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button button=view.findViewById(R.id.btnclickme);
+    }
+}
---
+++ app/src/main/java/edu/rupp/ckcc/fragment_demo/TabActivity.java
@@ -0,0 +1,57 @@
+package edu.rupp.ckcc.fragment_demo;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class TabActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+        BottomNavigationView bottomNavigationView=findViewById(R.id.buttonNavigationview);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        //Add home fragment to Activity
+        HomeFragment homeFragment=new HomeFragment();
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        transaction.replace(R.id.container,homeFragment);
+        transaction.commit();
+
+
+
+
+    }
+    //Event on bottom navigation view
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.home){
+            HomeFragment homeFragment=new HomeFragment();
+            ChangeFragment(homeFragment);
+        }
+        else if(item.getItemId()==R.id.profile){
+            ProfileFragment profileFragment=new ProfileFragment();
+            ChangeFragment(profileFragment);
+        }
+        else {
+            SettingFragment settingFragment=new SettingFragment();
+            ChangeFragment(settingFragment);
+        }
+        return false;
+    }
+    private void ChangeFragment(Fragment fragment){
+       FragmentManager  fragmentManager=getSupportFragmentManager();
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        transaction.replace(R.id.container,fragment);
+        transaction.commit();
+    }
+}
